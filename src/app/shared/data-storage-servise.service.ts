be7558462eb7e59131c1faae87543c6d9514a956
Yaroslav Onuchak <onuchak.j@gmail.com>
2021-08-31 14:52:47 +0300

    ку

--- src/app/shared/data-storage-servise.service.ts
+++ src/app/shared/data-storage-servise.service.ts
@@ -11,7 +11,7 @@ export class DataStorageServiseService {
 
   constructor(
     private http: HttpClient,
-              private recipeServise: RecipeServiseService) {
+    private recipeServise: RecipeServiseService) {
   }
 
 
