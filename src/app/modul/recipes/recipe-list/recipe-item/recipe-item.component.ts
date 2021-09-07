9adf71f6f91725b1c7175fb3b1d425a27dc5d526
Yaroslav Onuchak <onuchak.j@gmail.com>
2021-09-07 18:05:32 +0300

    auth 2

--- src/app/modul/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/modul/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -6,7 +6,7 @@ import {RecipeService} from "../../../../core/services/recipe.service";
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  // styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
